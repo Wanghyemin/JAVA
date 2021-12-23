81b5730c1efe79d466b982dbea16914488c0a9ef
Mini's note <95896024+Wanghyemin@users.noreply.github.com>
2021-12-23 23:45:29 +0900

    Add files via upload

--- demo/src/com/day2/Q6.java
+++ demo/src/com/day2/Q6.java
@@ -1,4 +1,4 @@
-package com.day2;
+package com.day1;
 
 import java.util.Scanner;
 
@@ -7,31 +7,27 @@ public class Q6 {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		
-		int a,b,c;
-		double d;
-		String name;
-		
+		int a, b, c, d;
+		String name, e;
+
 		System.out.print("이름?");
-		
 		name = sc.next();
-		
-		System.out.print("3과목 점수?");
-		
+
+		System.out.print("점수?");
 		a = sc.nextInt();
 		b = sc.nextInt();
 		c = sc.nextInt();
-		d = a+b+c;
-		
-		if(a>=40 && b>=40 && c>=40 && d>=180){
-			e = "합격";
-		}else if (a>=40 && b>=40 && c>=40 && d<=180){
+
+		d = a + b + c;
+
+		if (d >= 180) {
+			e = a >= 40 && b >= 40 && c >= 40 ? "합격" : "과락";
+		} else {
 			e = "불합격";
-		}else {
-			e = "과락";
 		}
-	
-		System.out.println("이름: " + name);
-		System.out.println("총점: "+d+", 평균: " + d/3 + ", 판정: " + e );
+
+		System.out.println("이름: " + name + "\n총점 : " + d + ", 평균: " + d / 3 + ", 판정: " + e);
+
 	}
+
 }
