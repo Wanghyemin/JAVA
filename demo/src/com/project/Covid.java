a697306a77e30025b9398005a24bc84e7ae94bce
Mini's note <95896024+Wanghyemin@users.noreply.github.com>
2022-01-19 18:07:20 +0900

    add

--- demo/src/com/project/Covid.java
+++ demo/src/com/project/Covid.java
@@ -3,6 +3,10 @@ package com.project;
 import java.io.InputStream;
 import java.net.URL;
 import java.text.NumberFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Scanner;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -13,134 +17,123 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-public class Covid{
-	NumberFormat numberFormat = NumberFormat.getInstance(); //3자리 수 마다 콤마찍기
-	DocumentBuilder parser;
-	int date;
+public class Covid {
+
+	Scanner sc = new Scanner(System.in);
+	NumberFormat numberFormat = NumberFormat.getInstance();
+	Calendar calendar = Calendar.getInstance();
+
+	double[] a = new double[2];
+	double[] b = new double[2];
+
+	String str1 = "";
+	String str2 = "";
+	String str3 = "";
+	String str4 = "";
 	
-	int a;
-	int b;
-	
-	
+	String today;
+
+	// 코로나 확진자, 사망자 공공API 데이터 가져오기 //
 	public void covid() {
 
 		try {
 
 			DocumentBuilderFactory f = DocumentBuilderFactory.newInstance();
-			parser = f.newDocumentBuilder();
+			DocumentBuilder parser = f.newDocumentBuilder();
+
+			//// 날짜설정 //// 
+			System.out.print(" 날짜를 입력하세요[20220101]");
+			today = sc.next(); 
 
 			
-
+			SimpleDateFormat formin = new SimpleDateFormat("yyyyMMdd");
 			
-			// 날짜설정
-			Scanner sc = new Scanner(System.in);
+			// 오늘
+			Date todayDate = formin.parse(today);
+			calendar.setTime(todayDate);
 			
+			// 내일
+			calendar.add(Calendar.DATE,1);
+			String tomorrow = formin.format(calendar.getTime());
+			
+			calendar.setTime(todayDate);
+			
+			//// URL 데이터 읽기 ////
+			//  배열 day = {  전일 ,  당일 };
+			String[] day = { today, tomorrow };
 
-			do {
-				System.out.println("날짜를 입력하세요[20220101]");
-				date = sc.nextInt();
+			for (int i = 0; i < day.length; i++) {
 				
-			} while (date < 20200101 || date > 20220119);
+				String url = "http://openapi.data.go.kr/openapi/service/rest/Covid19"
+						+ "/getCovid19InfStateJson?serviceKey=NCIUqcH3S7%2B53YimC1Rc"
+						+ "H3h3AiFSpZrWZMfmjJGXFVZNGhf2BGW25igritgTfMcgMe4Gxd8qbS9nb%"
+						+ "2F6%2BwBycXw%3D%3D&pageNo=1&numOfRows=10&" 
+						+ "startCreateDt=" + day[i] + "&endCreateDt="+ day[i];
 
+				Document xmlDoc = null;
+				xmlDoc = parser.parse(url);
+
+				// 노드 읽기
+				Element root = xmlDoc.getDocumentElement();
+				Node body = root.getLastChild();
+				Node items = body.getFirstChild();
+				Node item = items.getFirstChild();
+				NodeList itemList = item.getChildNodes();
+
+				Node a1 = itemList.item(3);
+				Node b1 = itemList.item(2);
+
+				// 노드에 있는 값 Double로 받아오기
+				a[i] = Double.parseDouble(a1.getChildNodes().item(0).getNodeValue());
+				b[i] = Double.parseDouble(b1.getChildNodes().item(0).getNodeValue());
+
+			}
+
+			// 3자리 수 마다 콤마찍기 (111,111)
+			str1 = numberFormat.format(a[1]);
+			str2 = numberFormat.format(b[1]);
+
+			str3 = numberFormat.format(a[1] - a[0]);
+			str4 = numberFormat.format(b[1] - b[0]);
 			
 		} catch (Exception e) {
 			System.out.println(e.toString());
 		}
 	}
-	
-	
+
+	// 누적 코로나 확진자, 사망자 //
 	public void total() {
-		
-	try {
-		
-			// 당일자 URL형성
-			String url = "http://openapi.data.go.kr/openapi/service/rest/Covid19"
-					+ "/getCovid19InfStateJson?serviceKey=NCIUqcH3S7%2B53YimC1Rc"
-					+ "H3h3AiFSpZrWZMfmjJGXFVZNGhf2BGW25igritgTfMcgMe4Gxd8qbS9nb%"
-					+ "2F6%2BwBycXw%3D%3D&pageNo=1&numOfRows=10&" 
-					+ "startCreateDt=" + date + "&endCreateDt=" + date;
 
-			Document xmlDoc = null;
-			xmlDoc = parser.parse(url);
-
-			Element root = xmlDoc.getDocumentElement();
-
-			Node body = root.getLastChild();
-
-			Node items = body.getFirstChild();
-
-			Node item = items.getFirstChild();
-
-			NodeList itemList = item.getChildNodes();
-
-			Node a1 = itemList.item(3);
-			Node b1 = itemList.item(2);
-			
-
-
-			a = Integer.parseInt(a1.getChildNodes().item(0).getNodeValue());
-			b = Integer.parseInt(b1.getChildNodes().item(0).getNodeValue());
-			
-			double as = a; //a값을 double값으로 변환
-			double bs = b; 
-			String str1 = numberFormat.format(as); //3자리 수마다 콤마 찍어주기
-			String str2 = numberFormat.format(bs);
-			
-			
-			System.out.println("누적확진자: " + str1 + "명,  누적사망자: " + str2 + "명");
-		
-	} catch (Exception e) {
-		System.out.println(e.toString());
-	}
-	}
-			
-	public void daily() {	
-			
-		
-		try {
-			// 전일자 URL형성
-		String url = "http://openapi.data.go.kr/openapi/service/rest/Covid19"
-					+ "/getCovid19InfStateJson?serviceKey=NCIUqcH3S7%2B53YimC1Rc"
-					+ "H3h3AiFSpZrWZMfmjJGXFVZNGhf2BGW25igritgTfMcgMe4Gxd8qbS9nb%"
-					+ "2F6%2BwBycXw%3D%3D&pageNo=1&numOfRows=10&" + "startCreateDt=" 
-					+ (date - 1) + "&endCreateDt="
-					+ (date - 1);
-	
-		Document xmlDoc = null;
-			xmlDoc = parser.parse(url);
-
-			Element root = xmlDoc.getDocumentElement();
-
-			Node body = root.getLastChild();
-
-			Node items = body.getFirstChild();
-
-			Node item = items.getFirstChild();
-
-			NodeList itemList = item.getChildNodes();
-
-
-			Node c1 = itemList.item(3);
-			Node d1 = itemList.item(2);
-
-			int c = 0;
-			int d = 0;
-
-			c = Integer.parseInt(c1.getChildNodes().item(0).getNodeValue());
-			d = Integer.parseInt(d1.getChildNodes().item(0).getNodeValue());
-			
-			double hj = a-c; //확진자 값 double로 변환
-			double sm = b-d;
-	
-			String str3 = numberFormat.format(hj); //확진자 값에 콤마 찍기
-			String str4 = numberFormat.format(sm);
-			
-			System.out.println("일일확진자: " + str3 + "명,  일일사망자: " + str4 + "명");
-
-		} catch (Exception e) {
-			System.out.println(e.toString());
+		if (str1 == "" || str2 == "") { //날짜 값이 안들어왔으면 리턴
+			return;
 		}
+		
+		SimpleDateFormat formout = new SimpleDateFormat("yyyy년 MM월 dd일"); //캘린더 날짜 포맷 변경
+		String today = formout.format(calendar.getTime());
+		
+		System.out.println("\n\n ★ 2020년 01월 20일부터 "+today+"까지 ");
+		System.out.println("\n   - 총 확진자: " + str1 + "명\n\n   - 총 사망자: " + str2 + "명\n");
+		
+		str1 = "";
+		str2 = "";
 
+	}
+
+	// 일일 코로나 확진자, 사망자 //
+	public void daily() {
+		
+		if (str3 == "" || str4 == "") { //날짜 값이 안들어왔으면 리턴
+			return;
+		}
+		
+		SimpleDateFormat formout = new SimpleDateFormat("yyyy년 MM월 dd일"); //캘린더 날짜 포맷 변경
+		String today = formout.format(calendar.getTime());
+		System.out.println("\n\n ★ "+today+" 기준 ");
+		System.out.println("\n   - 일일 확진자: " + str3 + "명\n\n   - 일일 사망자: " + str4 + "명\n");
+
+		str3 = "";
+		str4 = "";
+		
 	}
 
 }
